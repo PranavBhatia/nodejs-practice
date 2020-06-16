57201987f8cd7b45be09d83216d02834d09946ec
PranavBhatia <bhatiapranav1996@gmail.com>
2020-06-15 23:08:38 -0400

    Routing Requests

--- 2. Basics/app.js
+++ 2. Basics/app.js
@@ -4,14 +4,20 @@ const http = require('http');
 // http.createServer(requestListner);
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
     res.setHeader('Content-Type', 'text/html');
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.write('<html>');
     res.write('<head><title>My first page</title></head>');
     res.write('<body><h1>Hello from my Node.js server!</h1></body>');
     res.write('</html>');
     res.end();
-    // process.exit();
 });
 
 server.listen(3000);
