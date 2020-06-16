99b319b7962f3180d7dd7619f544d7c3a3510ff2
Pranav Bhatia <pranav.bhatia@sinopetech.com>
2020-06-16 15:45:59 -0400

    Adding a 404 Error Page

--- 3. Express.js/app.js
+++ 3. Express.js/app.js
@@ -1,15 +1,18 @@
 const express = require("express");
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 const bodyParser = require("body-parser");
 
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
