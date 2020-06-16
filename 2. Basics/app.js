ef7838c12c925dd7adce0e3c652fe0eb1feacbf9
PranavBhatia <bhatiapranav1996@gmail.com>
2020-06-15 23:15:26 -0400

    Redirecting Requests

--- 2. Basics/app.js
+++ 2. Basics/app.js
@@ -1,16 +1,22 @@
 const http = require('http');
-
-// function requestListner(req, res) {}
-// http.createServer(requestListner);
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     res.setHeader('Content-Type', 'text/html');
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') { 
+        fs.writeFileSync('message.txt', 'DUMMY');
+        // res.writeHead(302, {Location: '/'});
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.write('<html>');
